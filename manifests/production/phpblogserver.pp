1e441a71bb74bb6a0d4cf8a1a8c25015c0df70a8
root <root@ip-172-31-12-86.us-west-1.compute.internal>
2014-07-14 10:41:10 +0000

    Now the whole picture is relatively complete... few additional function variables need to be written.

---
+++ manifests/production/phpblogserver.pp
@@ -0,0 +1,9 @@
+
+
+node "ec2-54-215-248-123.us-west-1.compute.amazonaws.com" {
+	include profile::webphpblog
+}
+
+node "ec2-54-215-250-233.us-west-1.compute.amazonaws.com" {
+	include profile::webphpblog
+}
